f223d215c6ca538bb05ee840fdd9915e37ad2980
Daniel Rozo <drozo@vistaprint.com>
2015-08-20 15:39:32 +0200

    Removing unnecessary entities

--- Ing2Ynab/Converters/Entities/DescriptionToPayeeConversionRule.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Ing2Ynab.Converters.Entities
-{
-    internal class DescriptionToPayeeConversionRule
-    {
-        public string DescriptionContains { get; set; }
-        public string PayeeReplacement { get; set; }
-    }
-}
